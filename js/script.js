9ec3efa72b65fed81ce831abec1433502f3b1348
Brent Carton <brent.carton@student.ehb.be>
2020-05-21 15:09:31 +0200

    doelgroepfilter done

--- js/script.js
+++ js/script.js
@@ -1,37 +1,62 @@
 "use strict"
 $(function () {
+    let filters = {
+        genre: [""],
+        doelgroep: [""]
+    };
     $.ajax({
         url: "entries.json",
         method: "GET",
         dataType: "json"
     }).done(function (data) {
-        getDoelgroep(data);
-        getGenre(data);
+        useFilters(data.items, filters);
+        getFilters(data.items, filters);
     }).fail(function (a, b) {
         console.log(a, b);
     });
 })
 
-function getDoelgroep(data) {
-    let doelgroepFilters = []
-    $(".doelgroep").click(function () {
-        getFilters(this, doelgroepFilters);
-    })
-}
-
-function getGenre(data) {
+function getFilters(data, filters) {
     let genreFilters = []
     $(".genre").click(function () {
-        getFilters(this, genreFilters);
+        genreFilters = setFilters(this, genreFilters);
+        filters.genre.splice(0, 1);
+        filters.genre.push(genreFilters);
+        //console.log(filters);
+        useFilters(data, filters);
+    })
+    let doelgroepFilters = []
+    $(".doelgroep").click(function () {
+        doelgroepFilters = setFilters(this, doelgroepFilters);
+        filters.doelgroep.splice(0, 1);
+        filters.doelgroep.push(doelgroepFilters);
+        //console.log(filters);
+        useFilters(data, filters);
     })
 }
 
-function getFilters(filterSoort, filters) {
+function setFilters(filterSoort, filters) {
     let filter = filterSoort.value;
     if (filterSoort.checked) {
         filters.push(filter);
     } else {
         filters.splice(filters.indexOf(filter), 1);
     }
-    console.log(filters);
+    return filters;
+}
+
+function useFilters(data, filters) {
+    let doelgroepFilters = filters.doelgroep;
+    let genreFilters = filters.genre;
+    if (doelgroepFilters[0].length == 0 && genreFilters[0].length == 0) {
+        console.log(data);
+    } else {
+        for (let dataIndex in data) {
+            for (let doelGroepIndex in data) {
+                if (data[dataIndex].category == doelgroepFilters[0][doelGroepIndex]) {
+                    console.log(data[dataIndex]);
+                }
+            }
+        }
+    }
 }

fe9f4afd5d68329441fcefd8314c2159c84861b3
Brent Carton <brent.carton@student.ehb.be>
2020-05-24 20:22:13 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ $(function () {
     });
 })
 
+//Wanneer er op een filter wordt geklikt wordt de vorige filterarray verwijdert, alsook alle items die op dat moment
+//worden weergegeven en wordt de nieuwe filterarray toegevoegd aan het object. Die wordt dan doorgegeven aan een andere
+//functie waar deze verwerkt zullen worden. 
+
 function getFilters(data, filters) {
     let genreFilters = []
     $(".genre").click(function () {
@@ -37,6 +41,9 @@ function getFilters(data, filters) {
     })
 }
 
+//Deze functie checked welke filters zijn aangeduid en pushed ze in de filterarray. Indien men de filter nog eens aanclicked
+//zal deze gedeselecteerd worden en verwijdert worden uit de filterarray.
+
 function setFilters(filterSoort, filters) {
     let filter = filterSoort.value;
     if (filterSoort.checked) {
@@ -47,10 +54,14 @@ function setFilters(filterSoort, filters) {
     return filters;
 }
 
+// In deze functie worden de items gefilterd volgens de verschillende criteria/situaties (zie readme file). Elk item die
+//de aangeduide filters matched zal worden doorgegeven aan een nieuwe functie die deze verder verwerkt.
+
 function useFilters(data, filters) {
     let doelgroepFilters = filters.doelgroep;
     let genreFilters = filters.genre;
-    let test = 0;
+
+    //Indien er geen filters zijn aangeduid worden alle items weergegeven.
     if (doelgroepFilters[0].length == 0 && genreFilters[0].length == 0) {
         console.log("geen");
         console.log(data);
@@ -58,6 +69,7 @@ function useFilters(data, filters) {
             appendToHTML(data[dataIndex]);
         }
     } else {
+        //Indien enkel genrefilters zijn aangeduid filter ik enkel op genre.
         if (doelgroepFilters[0].length == 0) {
             console.log("genre");
             for (let dataIndex in data) {
@@ -68,6 +80,7 @@ function useFilters(data, filters) {
 
                 }
             }
+            //Indien enkel doelgroepfilters zijn aangeduid filter ik enkel op doelgroep
         } else if (genreFilters[0].length == 0) {
             console.log("doelgroep");
             for (let dataIndex in data) {
@@ -77,6 +90,7 @@ function useFilters(data, filters) {
                     }
                 }
             }
+            //Indien beide filters zijn aangeduid zal ik eerst alle items filteren op doelgroep en vervolgens op genre
         } else {
             console.log("beide");
             for (let dataIndex in data) {
@@ -91,6 +105,8 @@ function useFilters(data, filters) {
         }
     }
 }
+
+//Deze functie zal alle items die voldoen aan de filters toevoegen aan de html file
 
 function appendToHTML(data) {
     $(".content").append(`<div class="card"><div class="imgContainer"><img src="${data.thumbnail.url}" alt="${data.slug}"></div><h2>${data.name}</h2><h3 class="genreTitle">${data["genre-v2"]}</h3><h3 class="leeftijdTitle">${data.age}</h3><p>${data.excerpt}</p><p>${data["recorded-at"]}</p><h4>${data["video-length"]}</h4></div>`)
